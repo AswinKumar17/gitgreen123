cc2bcd4eebf34c9579d360974ef87246352c5c49
AswinKumar17 <aswinkumar1710@gmail.com>
2022-04-27 12:50:45 +0530

    Message1

---
+++ src/FirstGit/Degree.java
@@ -0,0 +1,16 @@
+package FirstGit;
+
+public class Degree {
+	public void bsc() {
+		System.out.println("Bachelor of Science");
+		}
+		public void bEd() {
+		System.out.println("Bachelor of Education");
+		}
+		public void bA() {
+		System.out.println("Bachelor of Arts");
+		}
+		public void bBA() {
+		System.out.println("Bachelor of Business Administration");
+		}
+}
